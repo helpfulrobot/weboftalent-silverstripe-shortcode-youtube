6d37da51c24ede68a7195846e76519790704aba6
Gordon Anderson <gordon.b.anderson@gmail.com>
2015-12-16 15:19:16 +0700

    FIX: Use CamelCase as per convention for templates

--- code/YouTubeShortCodeHandler.php
+++ code/YouTubeShortCodeHandler.php
@@ -3,7 +3,7 @@
 class YouTubeShortCodeHandler {
 
 	// taken from http://www.ssbits.com/tutorials/2010/2-4-using-short-codes-to-embed-a-youtube-video/ and adapted for SS3
-	public static function parse_youtube($arguments, $caption = null, $parser = null) {
+	public static function parse_youtube($arguments, $caption = null, $parser = null, $tagName) {
 		// first things first, if we dont have a video ID, then we don't need to
 		// go any further
 		if (empty($arguments['id'])) {
@@ -11,15 +11,24 @@ class YouTubeShortCodeHandler {
 		}
 
 		$customise = array();
-		/*** SET DEFAULTS ***/
+
+		// YouTube video id
 		$customise['YouTubeID'] = $arguments['id'];
+
 		//play the video on page load
-		$customise['autoplay'] = false;
+		$set = isset($arguments['autoplay']);
+		$customise['AutoPlay'] = $set ? true : false;
+
 		//set the caption
-		$customise['caption'] = $caption ? Convert::raw2xml($caption) : false;
+		$customise['Caption'] = $caption ? Convert::raw2xml($caption) : false;
+
 		//set dimensions
-		$customise['width'] = 560;
-		$customise['height'] = 315;
+		$widthSet = isset($arguments['width']);
+		$heightSet = isset($arguments['height']);
+		$customise['Width'] = $widthSet ? $arguments['width'] : 560;
+		$customise['Height'] = $heightSet ? $arguments['height'] : 315;
+
+		error_log(print_r($customise,1));
 
 		//overide the defaults with the arguments supplied
 		$customise = array_merge($customise, $arguments);
